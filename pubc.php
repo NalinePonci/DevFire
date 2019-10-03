312d78eead0bc7623643932b9ceb3c568aa57e05
LeoPonci <48107902+NalinePonci@users.noreply.github.com>
2019-10-02 22:41:34 -0300

    UPGRAND

---
+++ pubc.php
@@ -0,0 +1,22 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/estilolinux.css" type="text/css">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    require_once 'navLinux.php';
+    ?>
+    <div class="container">
+
+    </div>
+</body>
+
+</html>
